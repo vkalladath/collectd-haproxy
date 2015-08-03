d890690fc9b313c13f8a8c003cee16c3ca2f4cfa
Matthew Pound <mwp@signalfx.com>
2015-08-03 11:26:40 -0700

    update to modern version of haproxy

--- haproxy.py
+++ haproxy.py
@@ -3,184 +3,214 @@
 # Author: Michael Leinartas
 # Description: This is a collectd plugin which runs under the Python plugin to
 # collect metrics from haproxy.
-# Plugin structure and logging func taken from https://github.com/phrawzty/rabbitmq-collectd-plugin
+# Plugin structure and logging func taken from
+# https://github.com/phrawzty/rabbitmq-collectd-plugin
+#
+# Modified by "Warren Turkal" <wt@signalfuse.com>
 
-import collectd
+import cStringIO as StringIO
 import socket
 import csv
 
-NAME = 'haproxy'
+import collectd
+
+PLUGIN_NAME = 'haproxy'
 RECV_SIZE = 1024
 METRIC_TYPES = {
-  'bin': ('bytes_in', 'derive'),
-  'bout': ('bytes_out', 'derive'),
-  'chkfail': ('failed_checks', 'counter'),
-  'CurrConns': ('connections', 'gauge'),
-  'downtime': ('downtime', 'counter'),
-  'dresp': ('denied_response', 'derive'),
-  'dreq': ('denied_request', 'derive'),
-  'econ': ('error_connection', 'derive'),
-  'ereq': ('error_request', 'derive'),
-  'eresp': ('error_response', 'derive'),
-  'hrsp_1xx': ('response_1xx', 'derive'),
-  'hrsp_2xx': ('response_2xx', 'derive'),
-  'hrsp_3xx': ('response_3xx', 'derive'),
-  'hrsp_4xx': ('response_4xx', 'derive'),
-  'hrsp_5xx': ('response_5xx', 'derive'),
-  'hrsp_other': ('response_other', 'derive'),
-  'PipesUsed': ('pipes_used', 'gauge'),
-  'PipesFree': ('pipes_free', 'gauge'),
-  'qcur': ('queue_current', 'gauge'),
-  'Tasks': ('tasks', 'gauge'),
-  'Run_queue': ('run_queue', 'gauge'),
-  'rate': ('session_rate', 'gauge'),
-  'req_rate': ('request_rate', 'gauge'),
-  'stot': ('session_total', 'counter'),
-  'scur': ('session_current', 'gauge'),
-  'wredis': ('redistributed', 'derive'),
-  'wretr': ('retries', 'counter'),
-  'Uptime_sec': ('uptime_seconds', 'counter')
+    'MaxConn': ('max_connections', 'gauge'),
+    'CumConns': ('connections', 'counter'),
+    'CumReq': ('requests', 'counter'),
+    'MaxConnRate': ('max_connection_rate', 'gauge'),
+    'MaxSessRate': ('max_session_rate', 'gauge'),
+    'MaxSslConns': ('max_ssl_connections', 'gauge'),
+    'CumSslConns': ('ssl_connections', 'counter'),
+    'MaxSslConns': ('max_ssl_connections', 'gauge'),
+    'MaxPipes': ('max_pipes', 'gauge'),
+    'Idle_pct': ('idle_pct', 'gauge'),
+    'Tasks': ('tasks', 'gauge'),
+    'Run_queue': ('run_queue', 'gauge'),
+    'PipesUsed': ('pipes_used', 'gauge'),
+    'PipesFree': ('pipes_free', 'gauge'),
+    'Uptime_sec': ('uptime_seconds', 'counter'),
+    'bin': ('bytes_in', 'counter'),
+    'bout': ('bytes_out', 'counter'),
+    'chkfail': ('failed_checks', 'counter'),
+    'downtime': ('downtime', 'counter'),
+    'dresp': ('denied_response', 'counter'),
+    'dreq': ('denied_request', 'counter'),
+    'econ': ('error_connection', 'counter'),
+    'ereq': ('error_request', 'counter'),
+    'eresp': ('error_response', 'counter'),
+    'hrsp_1xx': ('response_1xx', 'counter'),
+    'hrsp_2xx': ('response_2xx', 'counter'),
+    'hrsp_3xx': ('response_3xx', 'counter'),
+    'hrsp_4xx': ('response_4xx', 'counter'),
+    'hrsp_5xx': ('response_5xx', 'counter'),
+    'hrsp_other': ('response_other', 'counter'),
+    'qcur': ('queue_current', 'gauge'),
+    'rate': ('session_rate', 'gauge'),
+    'req_rate': ('request_rate', 'gauge'),
+    'stot': ('session_total', 'counter'),
+    'scur': ('session_current', 'gauge'),
+    'wredis': ('redistributed', 'counter'),
+    'wretr': ('retries', 'counter'),
 }
 
-METRIC_DELIM = '.' # for the frontend/backend stats
+METRIC_DELIM = '.'  # for the frontend/backend stats
 
 DEFAULT_SOCKET = '/var/lib/haproxy/stats'
-DEFAULT_PROXY_MONITORS = [ 'server', 'frontend', 'backend' ]
 VERBOSE_LOGGING = False
+HAPROXY_SOCKET = None
+
+
+class Logger(object):
+    def error(self, msg):
+        collectd.error('{name}: {msg}'.format(name=PLUGIN_NAME, msg=msg))
+
+    def notice(self, msg):
+        collectd.warning('{name}: {msg}'.format(name=PLUGIN_NAME, msg=msg))
+
+    def warning(self, msg):
+        collectd.notice('{name}: {msg}'.format(name=PLUGIN_NAME, msg=msg))
+
+    def verbose(self, msg):
+        if VERBOSE_LOGGING:
+            collectd.info('{name}: {msg}'.format(name=PLUGIN_NAME, msg=msg))
+
+log = Logger()
+
 
 class HAProxySocket(object):
-  def __init__(self, socket_file=DEFAULT_SOCKET):
-    self.socket_file = socket_file
+    def __init__(self, socket_file=DEFAULT_SOCKET):
+        self.socket_file = socket_file
 
-  def connect(self):
-    s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    s.connect(self.socket_file)
-    return s
+    def connect(self):
+        stat_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        stat_sock.connect(self.socket_file)
+        return stat_sock
 
-  def communicate(self, command):
-    ''' Send a single command to the socket and return a single response (raw string) '''
-    s = self.connect()
-    if not command.endswith('\n'): command += '\n'
-    s.send(command)
-    result = ''
-    buf = ''
-    buf = s.recv(RECV_SIZE)
-    while buf:
-      result += buf
-      buf = s.recv(RECV_SIZE)
-    s.close()
-    return result
+    def communicate(self, command):
+        '''Get response from single command.
 
-  def get_server_info(self):
-    result = {}
-    output = self.communicate('show info')
-    for line in output.splitlines():
-      try:
-        key,val = line.split(':')
-      except ValueError, e:
-        continue
-      result[key.strip()] = val.strip()
-    return result
+        Args:
+            command: string command to send to haproxy stat socket
 
-  def get_server_stats(self):
-    output = self.communicate('show stat')
-    #sanitize and make a list of lines
-    output = output.lstrip('# ').strip()
-    output = [ l.strip(',') for l in output.splitlines() ]
-    csvreader = csv.DictReader(output)
-    result = [ d.copy() for d in csvreader ]
-    return result
+        Returns:
+            a string of the response data
+        '''
+        if not command.endswith('\n'):
+            command += '\n'
+        stat_sock = self.connect()
+        stat_sock.sendall(command)
+        result_buf = StringIO.StringIO()
+        buf = stat_sock.recv(RECV_SIZE)
+        while buf:
+            result_buf.write(buf)
+            buf = stat_sock.recv(RECV_SIZE)
+        stat_sock.close()
+        return result_buf.getvalue()
+
+    def get_server_info(self):
+        result = {}
+        output = self.communicate('show info')
+        for line in output.splitlines():
+            try:
+                key, val = line.split(':', 1)
+            except ValueError:
+                continue
+            result[key.strip()] = val.strip()
+        return result
+
+    def get_server_stats(self):
+        output = self.communicate('show stat')
+        #sanitize and make a list of lines
+        output = output.lstrip('# ').strip()
+        output = [l.strip(',') for l in output.splitlines()]
+        csvreader = csv.DictReader(output)
+        result = [d.copy() for d in csvreader]
+        return result
+
 
 def get_stats():
-  stats = dict()
-  haproxy = HAProxySocket(HAPROXY_SOCKET)
+    if HAPROXY_SOCKET is None:
+        return
 
-  try:
-    server_info = haproxy.get_server_info()
-    server_stats = haproxy.get_server_stats()
-  except socket.error, e:
-    logger('warn', "status err Unable to connect to HAProxy socket at %s" % HAPROXY_SOCKET)
+    stats = {}
+    haproxy = HAProxySocket(HAPROXY_SOCKET)
+
+    try:
+        server_info = haproxy.get_server_info()
+        server_stats = haproxy.get_server_stats()
+    except socket.error:
+        log.warning(
+            'status err Unable to connect to HAProxy socket at %s' %
+            HAPROXY_SOCKET)
+        return stats
+
+    for key, val in server_info.iteritems():
+        try:
+            stats[key] = int(val)
+        except (TypeError, ValueError):
+            pass
+
+    ignored_svnames = set(['BACKEND'])
+    for statdict in server_stats:
+        if statdict['svname'] in ignored_svnames:
+            continue
+        for key, val in statdict.items():
+            metricname = METRIC_DELIM.join(
+                [statdict['svname'].lower(), statdict['pxname'].lower(), key])
+            try:
+                stats[metricname] = int(val)
+            except (TypeError, ValueError):
+                pass
     return stats
 
-  if 'server' in PROXY_MONITORS:
-    for key,val in server_info.items():
-      try:
-        stats[key] = int(val)
-      except (TypeError, ValueError), e:
-        pass
-
-  for statdict in server_stats:
-    if not (statdict['svname'].lower() in PROXY_MONITORS or statdict['pxname'].lower() in PROXY_MONITORS):
-      continue
-    if statdict['pxname'] in PROXY_IGNORE:
-      continue
-    for key,val in statdict.items():
-      metricname = METRIC_DELIM.join([ statdict['svname'].lower(), statdict['pxname'].lower(), key ])
-      try:
-        stats[metricname] = int(val)
-      except (TypeError, ValueError), e:
-        pass
-  return stats
 
 def configure_callback(conf):
-  global PROXY_MONITORS, PROXY_IGNORE, HAPROXY_SOCKET, VERBOSE_LOGGING
-  PROXY_MONITORS = [ ]
-  PROXY_IGNORE = [ ]
-  HAPROXY_SOCKET = DEFAULT_SOCKET
-  VERBOSE_LOGGING = False
+    global HAPROXY_SOCKET, VERBOSE_LOGGING
+    HAPROXY_SOCKET = DEFAULT_SOCKET
+    VERBOSE_LOGGING = False
 
-  for node in conf.children:
-    if node.key == "ProxyMonitor":
-      PROXY_MONITORS.append(node.values[0])
-    elif node.key == "ProxyIgnore":
-      PROXY_IGNORE.append(node.values[0])
-    elif node.key == "Socket":
-      HAPROXY_SOCKET = node.values[0]
-    elif node.key == "Verbose":
-      VERBOSE_LOGGING = bool(node.values[0])
-    else:
-      logger('warn', 'Unknown config key: %s' % node.key)
+    for node in conf.children:
+        if node.key == "Socket":
+            HAPROXY_SOCKET = node.values[0]
+        elif node.key == "Verbose":
+            VERBOSE_LOGGING = bool(node.values[0])
+        else:
+            log.warning('Unknown config key: %s' % node.key)
 
-  if not PROXY_MONITORS:
-    PROXY_MONITORS += DEFAULT_PROXY_MONITORS
-  PROXY_MONITORS = [ p.lower() for p in PROXY_MONITORS ]
 
 def read_callback():
-  logger('verb', "beginning read_callback")
-  info = get_stats()
+    log.verbose('beginning read_callback')
+    info = get_stats()
 
-  if not info:
-    logger('warn', "%s: No data received" % NAME)
-    return
+    if not info:
+        log.warning('%s: No data received' % PLUGIN_NAME)
+        return
 
-  for key,value in info.items():
-    key_prefix = ''
-    key_root = key
-    if not value in METRIC_TYPES:
-      try:
-        key_prefix, key_root = key.rsplit(METRIC_DELIM,1)
-      except ValueError, e:
-        pass
-    if not key_root in METRIC_TYPES:
-      continue
+    for key, value in info.iteritems():
+        key_prefix = ''
+        key_root = key
+        if not value in METRIC_TYPES:
+            try:
+                key_prefix, key_root = key.rsplit(METRIC_DELIM, 1)
+            except ValueError:
+                pass
+        if not key_root in METRIC_TYPES:
+            continue
 
-    key_root, val_type = METRIC_TYPES[key_root]
-    key_name = METRIC_DELIM.join([key_prefix, key_root])
-    val = collectd.Values(plugin=NAME, type=val_type)
-    val.type_instance = key_name
-    val.values = [ value ]
-    val.dispatch()
-
-def logger(t, msg):
-    if t == 'err':
-        collectd.error('%s: %s' % (NAME, msg))
-    elif t == 'warn':
-        collectd.warning('%s: %s' % (NAME, msg))
-    elif t == 'verb':
-        if VERBOSE_LOGGING:
-            collectd.info('%s: %s' % (NAME, msg))
-    else:
-        collectd.notice('%s: %s' % (NAME, msg))
+        key_root, val_type = METRIC_TYPES[key_root]
+        if key_prefix == '':
+            key_name = key_root
+        else:
+            key_name = METRIC_DELIM.join([key_prefix, key_root])
+        log.verbose('{0}: {1}'.format(key_name, value))
+        val = collectd.Values(plugin=PLUGIN_NAME, type=val_type)
+        val.type_instance = key_name
+        val.values = [value]
+        val.meta = {'bug_workaround': True}
+        val.dispatch()
 
 collectd.register_config(configure_callback)
 collectd.register_read(read_callback)
